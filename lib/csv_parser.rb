962b10596d33f0fb7eac9d252873cca4ced552c1
Andrea Nodari <andrea.nodari91@gmail.com>
2013-05-03 14:25:34 +0200

    options

--- lib/csv_parser.rb
+++ lib/csv_parser.rb
@@ -1,11 +1,35 @@
 require 'csv'
+require 'optparse'
 require_relative './ruby-svm-preprocessor'
 
 OPTIONS_INPUT  = { col_sep: "\t", headers: false }
 OUTPUT_DIR = File.expand_path "~/Desktop/projects/ruby-svm-preprocessor/ruby-svm/output"
 
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: ruby csv_parser.rb [options] <train> <test>"
+
+  opts.on("-m [TYPE]", "--mode [TYPE]", [:unigram, :bigram], "Select unigram (default) or bigram") do |mode|
+    options[:mode] = mode
+  end
+
+  opts.on("-s", "--stemming", "Use this you want stemming") do |s|
+    options[:stemming] = s
+  end
+
+  opts.on("-w", "--remove-stopwords", "Use this if you want remove stopwords") do |w|
+    options[:stopword] = w
+  end
+
+  opts.on("-l", "--language", "Select your language [LANG]") do |l|
+    options[:lang] = l
+  end
+
+end.parse!
+
 if ARGV.size != 2
-  puts "ruby csv_parser.rb <train> <test>"
+  puts "ruby csv_parser.rb [options] <train> <test>"
+  puts "ruby csv_parser.rb --help"
   exit 1
 end
 
@@ -14,7 +38,7 @@ input_train, input_test = ARGV
 output_train_path = "#{OUTPUT_DIR}/train.svm"
 output_test_path = "#{OUTPUT_DIR}/test.svm"
 
-processor = RubySVMPreprocessor.new
+processor = RubySVMPreprocessor.new(options)
 
 output_train = File.open(output_train_path, "w")
 output_test  = File.open(output_test_path, "w")
