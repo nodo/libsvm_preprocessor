df27cfa98fc08cc550eea2cbee93e19fd63ceb3b
Andrea Nodari <andrea.nodari91@gmail.com>
2013-05-03 13:04:04 +0200

    setting up test

--- lib/csv_parser.rb
+++ lib/csv_parser.rb
@@ -2,36 +2,33 @@ require 'csv'
 require_relative './ruby-svm-preprocessor'
 
 OPTIONS_INPUT  = { col_sep: "\t", headers: false }
-
+OUTPUT_DIR = '../output'
 
 if ARGV.size != 2
   puts "ruby csv_parser.rb <train> <test>"
   exit 1
 end
 
-train, test = ARGV
+input_train, input_test = ARGV
 
-input_train  = train
-output_train = "train.svm"
-
-#input_test   = test
-#output_test = "test.svm"
+output_train_path = "#{OUTPUT_DIR}/train.svm"
+output_test_path = "#{OUTPUT_DIR}/test.svm"
 
 processor = RubySVMPreprocessor.new
 
-output_train = File.open(output_train, "w")
-#output_test  = File.open(output_test, "w")
+output_train = File.open(output_train_path, "w")
+output_test  = File.open(output_test_path, "w")
 
 CSV.foreach(input_train, OPTIONS_INPUT) do |row|
   vector = processor.toSVM(processor.push(row))
   output_train.puts vector
 end
 
-# CSV.foreach(input_test, OPTIONS_INPUT) do |row|
-#   vector = (processor.push(row, false))
-#   output_test.puts processor.nice_string(vector)
-# end
+CSV.foreach(input_test, OPTIONS_INPUT) do |row|
+  vector = (processor.push(row, false))
+  output_test.puts processor.nice_string(vector)
+end
 
 output_train.close
-#output_test.close
+output_test.close
 
